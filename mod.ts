8de248a50b2534ec33e245ac945980ce60384bdd
catapart <catapart@outlook.com>
2024-07-15 15:29:32 -0500

    refactored into single file; added readme; added ts config;

--- mod.ts
+++
@@ -1,6 +0,0 @@
-import { DataRecord } from "./data-record.ts";
-import { RecordSetter, RecordSetterOptions, RecordProperty } from "./record-setter.ts";
-import { RecordStore, RecordStoreOptions } from "./record-store.ts";
-
-
-export { DataRecord, RecordSetter, type RecordSetterOptions, type RecordProperty, RecordStore, type RecordStoreOptions }
